dea4b9307be6ce5ac5b04159d2a50804de2662e2
Trí Nguyễn <trinm1996@gmail.com>
2023-08-11 17:51:04 +0700

    mmkv

---
+++ src/services/StorageMMKV.ts
@@ -0,0 +1,35 @@
+import {StateStorage} from "zustand/middleware";
+
+const MMKVS = require("react-native-mmkv")
+
+export const storage = MMKVS ? new MMKVS.MMKV() : null;
+
+const StorageMMKV: StateStorage = {
+  setItem: (name, value) => {
+    if (!storage) {
+      return
+    }
+    return storage.set(name, value)
+  },
+  getItem: (name) => {
+    if (!storage) {
+      return
+    }
+    const value = storage.getString(name)
+    return value ?? null
+  },
+  removeItem: (name) => {
+    if (!storage) {
+      return
+    }
+    return storage.delete(name)
+  },
+  removeAll: (name) => {
+    if (!storage) {
+      return
+    }
+    return storage.clearAll()
+  },
+};
+
+export default StorageMMKV;
